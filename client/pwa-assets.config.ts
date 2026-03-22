67e8d7201dec5b4c74db0513f910ab71ec04ddac
Ampoi <tange.toshihiro@outlook.jp>
2026-03-22 20:52:10 +0900

    PWAのアイコンに白が入り込むのを防止

--- client/pwa-assets.config.ts
+++ client/pwa-assets.config.ts
@@ -1,9 +1,29 @@
-import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config'
+import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config'
+import type { Preset } from '@vite-pwa/assets-generator/presets'
+
+const customPreset: Partial<Preset> = {
+  maskable: {
+    ...minimal2023Preset.maskable,
+    padding: 0
+  },
+  apple: {
+    ...minimal2023Preset.apple,
+    padding: 0
+  },
+  transparent: {
+    ...minimal2023Preset.transparent,
+    padding: 0
+  }
+}
+const preset: Preset = {
+  ...minimal2023Preset,
+  ...customPreset
+}
 
 export default defineConfig({
   headLinkOptions: {
     preset: '2023',
   },
   preset: preset,
-  images: ['public/icon.svg'],
+  images: ['public/logo.png']
 })
